67ae6707de0a02a8db18b1cb8cdc8e8b57df711e
Felix Seip <felixseip01@gmail.com>
2021-02-17 16:45:47 +0100

    Added user class for so that the jpa listener has something to do

---
+++ src/main/java/com/tutorial/felixseip/springevents/User.java
@@ -0,0 +1,28 @@
+package com.tutorial.felixseip.springevents;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class User {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private long id;
+
+  @Column
+  private String firstName;
+
+  @Column
+  private String lastName;
+
+  @Column
+  private String emailAddress;
+
+}
