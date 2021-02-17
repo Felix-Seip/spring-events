6d07cb2c75e7b1f4b56f2f002aaf5cca79bd55e0
Felix Seip <felixseip01@gmail.com>
2021-02-17 17:13:48 +0100

    Controller was a bit broken since I forgot to add the request mapping annotation to the class, which is actually what i wanted

--- src/main/java/com/tutorial/felixseip/springevents/EventController.java
+++ src/main/java/com/tutorial/felixseip/springevents/EventController.java
@@ -1,14 +1,26 @@
 package com.tutorial.felixseip.springevents;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller("/api/events")
+import com.tutorial.felixseip.springevents.events.User;
+import com.tutorial.felixseip.springevents.events.UserCreatedEvent;
+import com.tutorial.felixseip.springevents.events.publisher.UserCreatedEventPublisher;
+
+@Controller
+@RequestMapping("/api/events")
 public class EventController {
+
+  @Autowired
+  private UserCreatedEventPublisher userCreatedEventPublisher;
 
   @GetMapping("/publisher")
   public void publishEventWithSpringEventPublisher()  {
-
+    UserCreatedEvent userCreatedEvent = new UserCreatedEvent(this, User.builder().firstName("firstName").lastName("lastName")
+        .emailAddress("firstName@lastName.com").build());
+    userCreatedEventPublisher.publishEmailNotificationEvent(userCreatedEvent);
   }
 
   @GetMapping("/jpa")
