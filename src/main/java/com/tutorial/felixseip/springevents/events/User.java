dafca14ec346491d6c73decd697d8bcb6a601b1c
Felix Seip <felixseip01@gmail.com>
2021-02-17 17:21:10 +0100

    Modified user to have an event listener

--- src/main/java/com/tutorial/felixseip/springevents/events/User.java
+++ src/main/java/com/tutorial/felixseip/springevents/events/User.java
@@ -2,9 +2,12 @@ package com.tutorial.felixseip.springevents.events;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
+import com.tutorial.felixseip.springevents.events.listener.UserEventListener;
 
 import lombok.Builder;
 import lombok.Data;
@@ -12,6 +15,7 @@ import lombok.Data;
 @Data
 @Entity
 @Builder
+@EntityListeners(UserEventListener.class)
 public class User {
 
   @Id
