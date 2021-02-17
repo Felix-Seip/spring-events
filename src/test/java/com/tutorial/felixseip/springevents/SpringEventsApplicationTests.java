44db528dcbecb365042aaff95d74dbe1ec0622e5
Felix Seip <felixseip01@gmail.com>
2021-02-17 16:30:46 +0100

    initial commit

---
+++ src/test/java/com/tutorial/felixseip/springevents/SpringEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorial.felixseip.springevents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
