71ba7c0c5bd9c841eb5b7d5b1669c1a4c09aa122
Felix Seip <felixseip01@gmail.com>
2021-02-17 17:13:06 +0100

    Added builder to user to simplify the creation of the object

--- src/main/java/com/tutorial/felixseip/springevents/events/User.java
+++ src/main/java/com/tutorial/felixseip/springevents/events/User.java
@@ -6,10 +6,12 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Entity
+@Builder
 public class User {
 
   @Id
